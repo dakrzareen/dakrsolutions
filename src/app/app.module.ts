6c8a565e08c9a28787689b727171fb338ba1fe21
zareendakr <zareendakr@gmail.com>
2023-01-03 11:42:39 +0530

    first changes push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ServiceComponent } from './components/service/service.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServiceComponent,
+    ContactComponent,
+    AboutComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
